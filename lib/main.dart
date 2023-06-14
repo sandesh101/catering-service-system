8d44308e979e7102cd04903d68d213021cc47ffa
sandesh101 <rimal.sandesh11@gmail.com>
2023-06-14 22:16:27 +0545

    Created Home and Auth Screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:catering_service/view/home_page.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -9,6 +10,12 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      title: 'HotCase',
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: const HomePage(),
+    );
   }
 }
