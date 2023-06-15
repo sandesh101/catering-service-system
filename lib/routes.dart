b56336e2c6eb8831ad5a6e407ffa6ab5a911d005
sandesh101 <rimal.sandesh11@gmail.com>
2023-06-15 22:13:11 +0545

    Added Splash Screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,9 +1,11 @@
 import 'package:catering_service/view/home_page.dart';
 import 'package:catering_service/view/screen/auth/login_screen_ui.dart';
 import 'package:catering_service/view/screen/auth/sign_up_screen_ui.dart';
+import 'package:catering_service/view/screen/splash_screen/splash_screen.dart';
 
 final routeScreen = {
-  '/': (context) => const HomePage(),
+  '/': (context) => const SplashScreen(),
+  'home': (context) => const HomePage(),
   'login': (context) => const LoginPage(),
   'signup': (context) => const SignUpPage(),
 };
---
+++ lib/view/screen/splash_screen/splash_screen.dart
@@ -0,0 +1,43 @@
+import 'dart:async';
+
+import 'package:catering_service/constant.dart';
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Timer(const Duration(seconds: 2), () {
+      Navigator.pushNamedAndRemoveUntil(context, 'home', (route) => false);
+    });
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+            height: MediaQuery.of(context).size.height * 0.45,
+            width: MediaQuery.of(context).size.width,
+            image: const AssetImage('assets/images/logo.png'),
+          ),
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.05,
+          ),
+          Text("HotCase", style: AppTextStyle.normalText(fontSize: 32)),
+          Text("We Serves You The Best Food",
+              style: AppTextStyle.normalText(fontSize: 16)),
+        ],
+      ),
+    );
+  }
+}
