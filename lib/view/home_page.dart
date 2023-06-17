bbe5c16c9b061fdf4e07ec4cbe9ee947d89f2ef5
sandesh101 <rimal.sandesh11@gmail.com>
2023-06-17 11:39:14 +0545

    Changes

--- lib/view/home_page.dart
+++ lib/view/home_page.dart
@@ -3,7 +3,6 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:intl/intl.dart';
-// import 'package:intl/intl.dart';
 import 'package:nepali_date_picker/nepali_date_picker.dart';
 import 'package:nepali_date_picker/nepali_date_picker.dart' as picker;
 
@@ -44,9 +43,7 @@ class _HomePageState extends State<HomePage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        // backgroundColor: ColorConstant.primaryColor,
         appBar: AppBar(
-          // backgroundColor: ColorConstant.primaryColor,
           elevation: 2.0,
           title: Text(
             "HotCase",
