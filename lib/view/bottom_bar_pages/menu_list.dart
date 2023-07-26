f894f6cd5ac28ab23b90bbfa030984892453e39a
sandesh101 <rimal.sandesh11@gmail.com>
2023-07-26 09:23:07 +0545

    Unused Import Removed

--- lib/view/bottom_bar_pages/menu_list.dart
+++ lib/view/bottom_bar_pages/menu_list.dart
@@ -1,6 +1,5 @@
 import 'package:catering_service/constant.dart';
 import 'package:catering_service/provider/product_provider.dart';
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
