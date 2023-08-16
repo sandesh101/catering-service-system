702075d7363134bd60d17052f4230717d540b3ac
sandesh101 <rimal.sandesh11@gmail.com>
2023-08-16 17:22:25 +0545

    Cart/Order Fixes

--- lib/provider/cart_provider.dart
+++ lib/provider/cart_provider.dart
@@ -5,16 +5,44 @@ import 'package:flutter/material.dart';
 class CartProvider extends ChangeNotifier {
   User? currentUser = FirebaseAuth.instance.currentUser;
   List<Map<String, dynamic>> cartList = [];
+  bool _isUploading = false;
 
+  bool get isUploading => _isUploading;
+
+  setUploading(bool value) {
+    _isUploading = value;
+    notifyListeners();
+  }
+
+  Future getOrders() async {}
+
+  // Future uploadDataToFirebase(
+  //     String date, String foodItem, String numberPeople, bool isPacking) async {
+  //   CollectionReference ref = FirebaseFirestore.instance.collection('cart');
+  //   await ref.add({
+  //     'date': date,
+  //     'food_items': foodItem,
+  //     'number_of_people': numberPeople,
+  //     'packing': isPacking ? "Packing" : "In-House",
+  //   });
+  // }
   Future uploadDataToFirebase(
       String date, String foodItem, String numberPeople, bool isPacking) async {
+    setUploading(true);
     CollectionReference ref = FirebaseFirestore.instance.collection('cart');
-    ref.doc(currentUser!.uid).set({
-      'date': date,
-      'food_items': foodItem,
-      'number_of_people': numberPeople,
-      'packing': isPacking ? "Packing" : "In-House",
-    });
+
+    // Add a new document with an auto-generated ID
+    try {
+      await ref.add({
+        'user_id': currentUser!.uid,
+        'date': date,
+        'food_items': foodItem,
+        'number_of_people': numberPeople,
+        'packing': isPacking ? "Packing" : "In-House",
+      }).then((value) => setUploading(false));
+    } catch (e) {
+      print(e.toString());
+    }
   }
 
   // getValue() {
